731c3ed4fbdb238a88215ea6fcea0a7d11401d94
nolaprime <noelminwagaw@gmail.com>
2026-01-26 14:59:54 -0600

    Annotations added to repository classes.

--- driver-scoring-service/src/main/java/com/fleet/scoring/repository/DriverScoreRepository.java
+++ driver-scoring-service/src/main/java/com/fleet/scoring/repository/DriverScoreRepository.java
@@ -1,6 +1,8 @@
 package com.fleet.scoring.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DriverScoreRepository extends CrudRepository {
 }
